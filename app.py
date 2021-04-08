7bcf6d1fa5a8e5fcbbb631d1a94427460e5d8196
Sriram <invadersriram2002@gmail.com>
2021-04-08 11:36:31 +0530

    Added linkcheck() function and .env file with google API key

--- app.py
+++ app.py
@@ -3,11 +3,29 @@
 import discord
 from dotenv import load_dotenv
 import os
+from pysafebrowsing import SafeBrowsing
+import re
 
 load_dotenv()
 intents = discord.Intents.default()
 intents.members = True
 bot = discord.Client(intents=intents)
+
+def linkcheck(msg):
+    s = SafeBrowsing('GOOGLE_SAFE_BROWSING_API_KEY')
+    regex=r"(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’]))"
+    url=re.findall(regex,msg)
+    try:
+        link=url[0][0]
+        r = s.lookup_urls([link])
+        if r[link]["malicious"]==False:
+            return link+" **is not malicious**"
+        elif r[link]["malicious"]==True:
+            return link+" **is malicious**"
+        else:
+            return "Something's is wrong"
+    except:
+        return "There was no link in your command\nExample command: ``check <pastethelinkhere>``"
 
 # event listeners 
 @bot.event  
@@ -30,6 +48,9 @@ async def on_message(message):
 
     if message.content.startswith('hello'):
         await message.channel.send('Hello!')
+
+    if message.content.startswith('check'):
+        await message.channel.send(linkcheck(message.content))
 
 @bot.event
 async def on_member_join(member):
