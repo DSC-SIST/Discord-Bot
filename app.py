8b198eb6b5c3cbd5c10b4047615f9171b62debeb
Sriram <invadersriram2002@gmail.com>
2021-04-08 23:40:52 +0530

    Add link checking function

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 # import discord package 
 # pip install discord
 import discord
+from discord.ext import commands
 from dotenv import load_dotenv
 import os
 from pysafebrowsing import SafeBrowsing
@@ -9,23 +10,7 @@ import re
 load_dotenv()
 intents = discord.Intents.default()
 intents.members = True
-bot = discord.Client(intents=intents)
-
-def linkcheck(msg):
-    s = SafeBrowsing('GOOGLE_SAFE_BROWSING_API_KEY')
-    regex=r"(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’]))"
-    url=re.findall(regex,msg)
-    try:
-        link=url[0][0]
-        r = s.lookup_urls([link])
-        if r[link]["malicious"]==False:
-            return link+" **is not malicious**"
-        elif r[link]["malicious"]==True:
-            return link+" **is malicious**"
-        else:
-            return "Something's is wrong"
-    except:
-        return "There was no link in your command\nExample command: ``check <pastethelinkhere>``"
+bot=commands.Bot(command_prefix="$")
 
 # event listeners 
 @bot.event  
@@ -49,9 +34,6 @@ async def on_message(message):
     if message.content.startswith('hello'):
         await message.channel.send('Hello!')
 
-    if message.content.startswith('check'):
-        await message.channel.send(linkcheck(message.content))
-
 @bot.event
 async def on_member_join(member):
     guild_id = int(os.getenv('GUILD_ID'))
@@ -60,5 +42,25 @@ async def on_member_join(member):
     channel = guild.get_channel(channel_id)
     await channel.send(f"Welcome to the server {member.mention} ! :partying_face:")
     await member.send(f"Welcome to the {guild.name} server, {member.name}! :partying_face:")
+
+@bot.command(
+    name="checklink", 
+    brief="Use to check if a link is malicious or not", 
+    help="Uses google safe browsing API to check if a link is safe or not.\nHow to use:\n$checklink www.TheLink.com")
+async def checklinkfunc(ctx, link):
+    s = SafeBrowsing('GOOGLE_SAFE_BROWSING_API_KEY')
+    regex=r"(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’]))"
+    url=re.findall(regex,link)
+    try:
+        link=url[0][0]
+        r = s.lookup_urls([link])
+        if r[link]["malicious"]==False:
+            await ctx.channel.send(link+" **is not malicious**")
+        elif r[link]["malicious"]==True:
+            await ctx.channel.send(link+" **is malicious**")
+        else:
+            await ctx.channel.send("Something's is wrong")
+    except:
+        await ctx.channel.send("There was no link in your command\nExample command: ``check <pastethelinkhere>``")
 
 bot.run(os.getenv('TOKEN'))
