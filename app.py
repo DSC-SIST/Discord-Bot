8733ae688678b6637255971dcb3d7a1639b8c10e
Rajeev <rnayan516@gmail.com>
2021-04-12 14:22:01 +0530

    initial

--- app.py
+++ app.py
@@ -1,13 +1,34 @@
 # import discord package 
 # pip install discord
+import datetime
 import discord
 from dotenv import load_dotenv
 import os
+from wordnik import *
 
 load_dotenv()
 intents = discord.Intents.default()
 intents.members = True
 bot = discord.Client(intents=intents)
+
+def wordOfDay():
+	now=datetime.datetime.now()
+	today=str(now.year)+'-'+str(now.month)+'-'+str(now.day)
+	url='http://api.wordnik.com:80/v4/words.json/wordOfTheDay?date='+today+'&api_key=d52b63b6880f17811310d0fbd3b0d3a8ef163a248f58dc831'
+	response=urllib.urlopen(url)
+	word=json.load(response)
+	x=word['word']
+	word_of_the_day=bytes(x)
+	y=word['note']
+	note=bytes(y)
+	meaning=word['definitions'][0]['text']
+	print "*****************************************"
+	print "The word of the day is: "+word_of_the_day
+	print "Meaning: "+meaning
+	print "Note : "+note
+	print "*****************************************"
+    result = f"The word of the day is: {word_of_the_day}"
+wordOfDay()
 
 # event listeners 
 @bot.event  
@@ -30,6 +51,10 @@ async def on_message(message):
 
     if message.content.startswith('hello'):
         await message.channel.send('Hello!')
+
+    if message.content == "$word":
+        res=wordOfDay()
+        await message.channel.send(res)
 
 @bot.event
 async def on_member_join(member):
