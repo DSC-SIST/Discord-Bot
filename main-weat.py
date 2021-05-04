076db16d874dd2fc19069bef6ec93a11684d0e2a
sivasai <57858904+sivasai9398@users.noreply.github.com>
2021-05-04 19:20:31 +0530

    Add files via upload

---
+++ main-weat.py
@@ -0,0 +1,39 @@
+import requests
+import discord
+from discord.ext import commands
+api_key = "71b0ae513cc0b2caabbaef60d40373b5"
+base_url = "http://api.openweathermap.org/data/2.5/weather?"
+client = commands.Bot(command_prefix='!')
+try:
+    @client.command()
+    async def weather(ctx, *, city: str):
+        city_name = city
+        complete_url = base_url + "appid=" + api_key + "&q=" + city_name
+        response = requests.get(complete_url)
+        x = response.json()
+        channel = ctx.message.channel
+        if x["cod"] != "404":
+            #async with channel.typing():
+            y = x["main"]
+            current_temperature = y["temp"]
+            current_temperature_celsiuis = str(round(current_temperature - 273.15))
+            current_pressure = y["pressure"]
+            current_humidity = y["humidity"]
+            z = x["weather"]
+            weather_description = z[0]["description"]
+            weather_description = z[0]["description"]
+            embed = discord.Embed(title=f"Weather in {city_name}",color=ctx.guild.me.top_role.color,timestamp=ctx.message.created_at,)
+            embed.add_field(name="Descripition", value=f"**{weather_description}**", inline=False)
+            embed.add_field(name="Temperature(C)", value=f"**{current_temperature_celsiuis}°C**", inline=False)
+            embed.add_field(name="Humidity(%)", value=f"**{current_humidity}%**", inline=False)
+            embed.add_field(name="Atmospheric Pressure(hPa)", value=f"**{current_pressure}hPa**", inline=False)
+            embed.set_thumbnail(url="https://i.ibb.co/CMrsxdX/weather.png")
+            embed.set_footer(text=f"Requested by {ctx.author.name}")
+            await channel.send(embed=embed)
+        else:
+            await channel.send("City not found ☹ . Try with some other place.")
+    @client.event
+    async def on_command_error(ctx,error):
+        if isinstance(error, commands.MissingRequiredArgument):
+            await ctx.send('Please Enter the place to find out the weather \n Type as follows:```!weather <Enter the place>```')
+
