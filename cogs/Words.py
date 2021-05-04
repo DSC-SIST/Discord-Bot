c66b06e165ae381de0e4720e5fdb51522cf47aac
Rajeev <rnayan516@gmail.com>
2021-05-04 16:26:42 +0530

    feat: add Word of the Day
    corrected code
    
    Modified Words.py File
    
    Refactored the code

--- cogs/Words.py
+++ cogs/Words.py
@@ -1,6 +1,3 @@
-# Built-In Libraries/Modules/Packages
-import os
-
 # Third Party Libraries/Modules/Packages
 from discord import Embed, Color
 from discord.ext import commands
@@ -8,54 +5,65 @@ import requests
 
 # User Defined Libraries/Modules/Packages
 from .utils.settings import Settings
+from .utils.words import getWordOfTheDay
 
-# Extending the commands.Cog class
+
 class Words(commands.Cog):
+    """
+    Extending the commands.Cog class for commands related to words.
+    """
 
-    # The constructor 
     def __init__(self, bot):
+        '''
+        The default constructor for the class: Words.
+        '''
         self.bot = bot
 
     @commands.command(
-        name = 'meaning',
-        aliases = ['what', 'def', 'define']
+        name='meaning',
+        aliases=['what', 'def', 'define']
     )
     async def getmeaning(self, ctx, word):
+        """
+        A user defined async function that returns the meaning
+        of the word passed by the user.
+        """
+
         DICTONARY_API = Settings().SECRETS['DICTIONARY_API']
         data = eval(requests.get(DICTONARY_API + str(word)).content.decode())
 
         if type(data) == "dict":
-            message = "Ooops. I could not find the meaning." 
+            message = "Ooops. I could not find the meaning."
             message += "Maybe try searching somewhere on web"
             message += " or checking the spelling??"
             ctx.send(message)
             return
 
         embed = Embed(
-            title = str(word).capitalize(),
-            color = Color.blue()
+            title=str(word).capitalize(),
+            color=Color.blue()
         )
         data = data[0]
 
         try:
             embed.add_field(
-                name = "Pronounciation",
-                value = data["phonetics"][0]["text"],
-                inline = True
+                name="Pronounciation",
+                value=data["phonetics"][0]["text"],
+                inline=True
             )
             embed.add_field(
-                name = "Hear it!",
-                value = data["phonetics"][0]["audio"],
-                inline = True
+                name="Hear it!",
+                value=data["phonetics"][0]["audio"],
+                inline=True
             )
         except:
             pass
 
         for i in data["meanings"]:
             embed.add_field(
-                name = str(i["partOfSpeech"]).capitalize(),
-                value = i["definitions"][0]["definition"],
-                inline = False
+                name=str(i["partOfSpeech"]).capitalize(),
+                value=i["definitions"][0]["definition"],
+                inline=False
             )
             try:
                 synstr = ''
@@ -66,16 +74,44 @@ class Words(commands.Cog):
                 synstr = synstr.strip()
                 synstr = synstr.strip(",")
                 embed.add_field(
-                    name = "Synonyms",
-                    value = synstr,
-                    inline = False
+                    name="Synonyms",
+                    value=synstr,
+                    inline=False
                 )
             except:
                 pass
 
-        await ctx.send(embed = embed)
+        await ctx.send(embed=embed)
         return
 
-# Following the docs for creating cogs
+    @commands.command(
+        name='wordoftheday',
+        aliases=['wotd']
+    )
+    async def wordOfDay(self, ctx):
+        """
+        A user defined async function that returns the word
+        of the day as an Embed object.
+        """
+
+        message = getWordOfTheDay()
+
+        embed = Embed(
+            title=message['word'],
+            color=Color.green()
+        )
+
+        embed.add_field(
+            name="Meaning",
+            value=message['meaning'],
+            inline=False
+        )
+
+        await ctx.send(embed=embed)
+
+
 def setup(bot):
+    '''
+    Following the docs for creating cogs
+    '''
     bot.add_cog(Words(bot))
