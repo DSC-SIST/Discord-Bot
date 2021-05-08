1929bfed864be2f1262b148b37c86c8e5851ab2a
Nannan <62479349+nanna7077@users.noreply.github.com>
2021-05-08 19:21:49 +0530

    Update Chat.py

--- cogs/Chat.py
+++ cogs/Chat.py
@@ -1,19 +1,39 @@
+# Third Party Libraries/Modules/Packages
 from discord.ext import commands
 
+
 class Chat(commands.Cog):
-    ''' For Chat based commands and events '''	
-    
+    """
+    Extending the commands.Cog class for Chat based commands and events.
+    """
+
     def __init__(self, bot):
+        '''
+        The default constructor for the class: Chat
+        '''
         self.bot = bot
 
     @commands.Cog.listener()
-    async def on_reaction_add(self, reaction, user):
-        if reaction.emoji == '📌' and user.id != self.bot.user.id:
-        # To prevent errors in case of lack of permission or HTTP Exception
+    async def on_raw_reaction_add(self, payload):
+        if payload.emoji.name == '\N{PUSHPIN}' and payload.user_id != self.bot.user.id:
+            # To prevent errors in case of lack of permission or HTTP Exception
             try:
-                await reaction.message.pin(reason='Requested by '+ str(user.id))
+                channel=self.bot.get_channel(payload.channel_id)
+                message=await channel.fetch_message(payload.message_id)
+                await message.pin(reason="Requested by "+str(payload.user_id))
             except:
                 pass
+        return
+        
+    @commands.Cog.listener()
+    async def on_raw_reaction_remove(self, payload):
+        if payload.emoji.name == '\N{PUSHPIN}' and payload.user_id != self.bot.user.id:
+            try:
+               channel=self.bot.get_channel(payload.channel_id)
+               message=await channel.fetch_message(payload.message_id)
+               await message.unpin(reason="Requested by "+str(payload.user_id))
+            except:
+               pass
         return
 
     @commands.Cog.listener()
@@ -32,13 +52,16 @@ class Chat(commands.Cog):
             except:
                 pass
         if before.pinned and not after.pinned:
-        # To prevent errors in case of lack of permission or HTTP Exception
+            # To prevent errors in case of lack of permission or HTTP Exception
             try:
-                await after.remove_reaction("📌",self.bot.user)
+                await after.remove_reaction("📌", self.bot.user)
             except:
                 pass
         return
 
+
 def setup(bot):
+    '''
+    Following the docs for creating cogs
+    '''
     bot.add_cog(Chat(bot))
-    
